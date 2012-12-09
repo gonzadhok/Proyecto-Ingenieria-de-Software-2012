60db98f1ec0225bfa9976b7422b43fc9e8c29e2b
Manuel <manast001@gmail.com>
2012-12-09 16:29:30 +0100

    fc:d0:56:a7:b1:e2:c2:ef:0a:4c:27:9c:a2:75:4f:12

--- Software/Conexion.php
+++ Software/Conexion.php
@@ -3,20 +3,23 @@
 include 'Funciones.php';
 
 function agregarNuevoPostulante($conexion, $informacion) {
-    $opciones = $informacion['CheckboxCarreras'];
+    $opciones = $informacion["CheckboxCarreras"];
     if (!buscarAlumno($informacion["rut"], $conexion) && count($opciones) > 0) {
         $datos = "$informacion[rut],'$informacion[nombre]','$informacion[apellido_paterno]','$informacion[apellido_materno]',$informacion[curso],'$informacion[colegio]','$informacion[comuna]',$informacion[telefono],'$informacion[correo]',NOW()";
         mysql_query("INSERT INTO ALUMNO VALUES($datos)", $conexion) or die('<META HTTP-EQUIV="REFRESH" CONTENT="5;URL=formulario.html">Datos mal ingresados');
         foreach ($opciones as $opcion) {
             $agregar = "INSERT INTO ALUMNO_CARRERA VALUES ($informacion[rut],$opcion)";
+            echo $agregar;
             mysql_query($agregar, $conexion);
         }
         echo '<script>alert("Formulario Guardado exitosamente")</script>';
         echo '<META HTTP-EQUIV="REFRESH" CONTENT="0;URL=formulario.html">';
     } elseif (count($opciones) < 0) {
-        echo '<META HTTP-EQUIV="REFRESH" CONTENT="5;URL=formulario.html">Seleccione Carreras que desea cursar';
+        echo '<script>alert("No se han agregado preferencias al postulante")</script>';
+        echo '<META HTTP-EQUIV="REFRESH" CONTENT="0;URL=formulario.html">';
     } else {
-        echo '<META HTTP-EQUIV="REFRESH" CONTENT="5;URL=formulario.html">El postulante ya se encuentra en la base de datos';
+        echo '<script>alert("El Postulante ya se encuentra en inscrito")</script>';
+        echo '<META HTTP-EQUIV="REFRESH" CONTENT="0;URL=formulario.html">';
     }
 }
 
